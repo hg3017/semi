8f5e0b967c08443718cc9f03b1ce7538d78c98bc
yunseo3489 <yunseo3489@gmail.com>
2024-12-12 15:36:12 +0900

    oo

--- src/main/java/com/semie/cook/controller/RecipeLabController.java
+++ src/main/java/com/semie/cook/controller/RecipeLabController.java
@@ -13,40 +13,44 @@ public class RecipeLabController {
 
     @GetMapping("/list")
     public String list(Model model) {
-        System.out.println("recipe_lab/list---------------------------------------------");
+        System.out.println("recipeLab/list---------------------------------------------");
         return "/recipeLab/recipe_lab";
     }
 
 
     @GetMapping("/solution")
     public String solution(Model model) {
-        System.out.println("recipe_lab/solution---------------------------------------------");
+        System.out.println("recipeLab/solution---------------------------------------------");
         return "/recipeLab/solution";
     }
 
     @GetMapping("/counseling")
     public String counseling(Model model) {
-        System.out.println("recipe_lab/counseling---------------------------------------------");
+        System.out.println("recipeLab/counseling---------------------------------------------");
         return "/recipeLab/counseling";
     }
 
     @GetMapping("/cooking")
     public String cooking(Model model) {
-        System.out.println("recipe_lab/cooking---------------------------------------------");
+        System.out.println("recipeLab/cooking---------------------------------------------");
         return "/recipeLab/cooking";
     }
 
     @GetMapping("/archive_rep")
     public String archive(Model model) {
-        System.out.println("recipe_lab/archive-----------------------------------------------");
+        System.out.println("recipeLab/archive-----------------------------------------------");
         return "/recipeLab/archive_rep";
     }
 
     @GetMapping("/archive_sol")
     public String archive_sol(Model model) {
-        System.out.println("recipe_lab/archive_sol-----------------------------------------------");
+        System.out.println("recipeLab/archive_sol-----------------------------------------------");
         return "/recipeLab/archive_sol";
     }
-
+    @GetMapping("/archive_cook")
+    public String archive_cook(Model model) {
+        System.out.println("recipeLab/archive_cook-----------------------------------------------");
+        return "/recipeLab/archive_cook";
+    }
 
 }
