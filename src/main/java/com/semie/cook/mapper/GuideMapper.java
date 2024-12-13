18e6800c5d6e7e499fd99a5100f7461fd5c65843
hg3017 <hyongyu1991@naver.com>
2024-12-13 18:20:22 +0900

    gyuuu

---
+++ src/main/java/com/semie/cook/mapper/GuideMapper.java
@@ -0,0 +1,13 @@
+package com.semie.cook.mapper;
+
+import com.semie.cook.common.Pagination;
+import com.semie.cook.model.GuideDTO;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+@Mapper
+public interface GuideMapper {
+    List<GuideDTO> selectAll(Pagination pg);
+    int totalGuide(Pagination pg);
+}
