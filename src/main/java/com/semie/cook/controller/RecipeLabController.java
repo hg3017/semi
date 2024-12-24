1d307cb2e3e5ce06e16e91daa2d60696e3df7331
yunseo3489 <yunseo3489@gmail.com>
2024-12-24 18:31:04 +0900

    커밋

--- src/main/java/com/semie/cook/controller/RecipeLabController.java
+++ src/main/java/com/semie/cook/controller/RecipeLabController.java
@@ -102,6 +102,10 @@ public class RecipeLabController {
         System.out.println("recipeLab/archive_counsel-----------------------------------------------");
         return "/recipeLab/archive_counsel";
     }
-
+    @GetMapping("/archive_write")
+    public String archive_write(Model model) {
+        System.out.println("recipeLab/archive_write-----------------------------------------------");
+        return "/recipeLab/archive_write";
+    }
 
 }
--- src/main/resources/static/js/recipe.js
+++ src/main/resources/static/js/recipe.js
@@ -102,7 +102,7 @@ window.addEventListener('DOMContentLoaded', function () {
 
     //레시피 상세
       let CtagList = new Swiper('.C_tagList .swiper', {
-      slidesPerView: '4',
+      slidesPerView: '3',
       navigation: {
           nextEl: '.C_tagList .swiper-button-next',
           prevEl: '.C_tagList .swiper-button-prev',
